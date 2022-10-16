c4b5cb8e6dc39af234bb2e107a4643e2b023d24c
Gaius Fonseca <gaiusfonseca@gmail.com>
2022-10-16 20:59:41 -0300

    implementando regras de validação para setar um id de workstation

--- src/main/java/br/gaius/models/Workstation.java
+++ src/main/java/br/gaius/models/Workstation.java
@@ -3,19 +3,32 @@ package br.gaius.models;
 public class Workstation {
 
 	private String id;
-	private String office;
-	private String floor;
 
-	public Workstation() {
-		
-	}
-	
-	public String getId() {
-		return id;
-	}
-	
-	public void setId(String id) {
+	public Workstation(String id) {
 		this.id = id;
 	}
-	
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        //verifica se o id possui 4 caarcteres
+        if(id.length() != 4) {
+            throw new IllegalArgumentException("O id deve conter 4 caracteres.");
+        }
+        
+        //verifica se o primeiro é letra
+        for(int i = 0; i <= 3; i++) {
+            if( i == 0 && !Character.isLetter(id.charAt(i))) {
+                throw new IllegalArgumentException("o primeiro dígito deve ser uma letra.");
+            }
+            
+            if(i != 0 && !Character.isDigit(id.charAt(i))) {
+                throw new IllegalArgumentException(String.format("O caractere na posição %d deveria ser um dígito", i));
+            }
+        }
+        
+        this.id = id;
+    }
 }
