c90dbdb5b9c867fa2810a7a9723c0c0bdb37cd78
Gaius Fonseca <gaiusfonseca@gmail.com>
2022-10-16 22:37:06 -0300

    escrevendo a interface de Scheduler

--- src/main/java/br/gaius/main/Scheduler.java
+++ src/main/java/br/gaius/main/Scheduler.java
@@ -3,18 +3,24 @@ package br.gaius.main;
 import java.util.ArrayList;
 import java.util.List;
 import br.gaius.models.Reservation;
-import br.gaius.models.User;
-import br.gaius.models.Workstation;
 
 public class Scheduler {
 
-    List<User> users;
-    List<Workstation> workstations;
     List<Reservation> reservations;
     
     public Scheduler() {
-        users = new ArrayList<User>();
-        workstations = new ArrayList<Workstation>();
         reservations = new ArrayList<Reservation>();
+    }
+    
+    public boolean  makeReservation(Reservation reservation) {
+        return false;
+    }
+    
+    public boolean removeReservation(Reservation reservation) {
+        return false;
+    }
+    
+    public List<Reservation> getReservationList(){
+        return reservations;
     }
 }
