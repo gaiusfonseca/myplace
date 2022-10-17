fb4f3cc7dbe1b40e7310f9f3c3730d7bce768bdd
Gaius Fonseca <gaiusfonseca@gmail.com>
2022-10-16 21:02:34 -0300

    escrevendo o algoritmo da classe

--- src/main/java/br/gaius/main/App.java
+++ src/main/java/br/gaius/main/App.java
@@ -3,6 +3,9 @@ package br.gaius.main;
 public class App{
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		//carregar todas as posições de trabalho
+	    //receuperar o status de cada uma das posições de trabalho
+	    //mostrar ao usuário o status das posições
+	    //usuário faz uma reserva em uma posição livre
 	}
 }
